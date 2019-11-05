2ab9dbeac1ee3f4995027fd139dc15d778a4923c
Uttam Sharma <uttams@bsf.io>
2019-11-05 15:12:46 +0530

    updated notice nonce

--- classes/class-uabb-iconfonts.php
+++ classes/class-uabb-iconfonts.php
@@ -90,7 +90,7 @@ class UABB_IconFonts {
 			@mkdir( $dst );
 		}
 
-		while ( false !== ( $file = readdir( $dir ) ) ) {
+		while ( false !== ( $file = readdir( $dir ) ) ) { // @codingStandardsIgnoreLine.
 			if ( ( '.' != $file ) && ( '..' != $file ) ) {
 				if ( is_dir( $src . '/' . $file ) ) {
 					$this->recurse_copy( $src . '/' . $file, $dst . '/' . $file );
