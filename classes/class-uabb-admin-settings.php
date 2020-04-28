e58c16b6c2c9f3ef364cbe0e9e5a4f3d7969ba2b
leenam <leenam@bsf.io>
2020-04-28 16:11:58 +0530

    added MONTH_IN_SECONDS constant

--- classes/class-uabb-admin-settings.php
+++ classes/class-uabb-admin-settings.php
@@ -118,11 +118,11 @@ final class UABBBuilderAdminSettings {
 					__( 'Would you please mind sharing your views and give it a 5 star rating on the WordPress repository?', 'ultimate-addon-for-beaver-builder' ),
 					'https://wordpress.org/support/plugin/ultimate-addons-for-beaver-builder-lite/reviews/?filter=5',
 					__( 'Ok, you deserve it', 'ultimate-addon-for-beaver-builder' ),
-					(2* WEEK_IN_SECONDS),
+					MONTH_IN_SECONDS,
 					__( 'Nope, maybe later', 'ultimate-addon-for-beaver-builder' ),
 					__( 'I already did', 'ultimate-addon-for-beaver-builder' )
 				),
-				'repeat-notice-after'        => (2* WEEK_IN_SECONDS),
+				'repeat-notice-after'        => MONTH_IN_SECONDS,
 				'display-notice-after'       => (2* WEEK_IN_SECONDS), // Display notice after 2 weeks
 				'priority'                   => 25,
 				'display-with-other-notices' => false,
