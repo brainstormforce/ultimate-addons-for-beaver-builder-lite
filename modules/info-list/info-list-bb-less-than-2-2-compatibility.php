53199b14f858d5a5d55df36a5553c32eeae4992a
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2024-06-27 12:49:38 +0530

    sanitized widgets for security

--- modules/info-list/info-list-bb-less-than-2-2-compatibility.php
+++ modules/info-list/info-list-bb-less-than-2-2-compatibility.php
@@ -233,6 +233,7 @@ FLBuilder::register_module(
 							'type'    => 'select',
 							'label'   => __( 'Select Tag', 'uabb' ),
 							'default' => 'h3',
+							'sanitize' => array( 'FLBuilderUtils::esc_tags', 'h4' ),
 							'options' => array(
 								'h1'   => __( 'H1', 'uabb' ),
 								'h2'   => __( 'H2', 'uabb' ),
--- modules/ribbon/ribbon-bb-less-than-2-2-compatibility.php
+++ modules/ribbon/ribbon-bb-less-than-2-2-compatibility.php
@@ -177,6 +177,7 @@ FLBuilder::register_module(
 							'type'    => 'select',
 							'label'   => __( 'Tag', 'uabb' ),
 							'default' => 'h3',
+							'sanitize' => array( 'FLBuilderUtils::esc_tags', 'h3' ),
 							'options' => array(
 								'h1'   => __( 'H1', 'uabb' ),
 								'h2'   => __( 'H2', 'uabb' ),
