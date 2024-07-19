90f196aee695063f0d8b543dbb002aa2aedd81be
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2024-07-19 17:06:24 +0530

    info table html tag

--- modules/info-table/includes/frontend.php
+++ modules/info-table/includes/frontend.php
@@ -16,10 +16,15 @@ if ( isset( $settings->it_link_nofollow ) ) {
 <?php } ?>
 <div class="uabb-module-content info-table-wrap info-table-<?php echo esc_attr( $settings->box_design ); ?> info-table-cs-<?php echo esc_attr( $settings->color_scheme ); ?>">
 	<div class="info-table">
+		<?php
+		// Define a whitelist of allowed tags
+			$allowed_tags  = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'span' );
+			$infotable_tag = in_array( $settings->heading_tag_selection, $allowed_tags ) ? $settings->heading_tag_selection : 'h3';
+		?>
 		<div class="info-table-heading">
-			<?php echo '<' . esc_attr( $settings->heading_tag_selection ) . " class='info-table-main-heading'>"; ?>
-			<?php echo esc_html( $settings->it_title );  ?>
-			<?php echo '</' . esc_attr( $settings->heading_tag_selection ) . '>'; ?>
+			<?php echo '<' . esc_attr( $infotable_tag ) . " class='info-table-main-heading'>"; ?>
+			<?php echo esc_html( $settings->it_title ); ?>
+			<?php echo '</' . esc_attr( $infotable_tag ) . '>'; ?>
 
 			<?php echo '<' . esc_attr( $settings->sub_heading_tag_selection ) . " class='info-table-sub-heading'>"; ?>
 			<?php echo esc_html( $settings->sub_heading ); ?>
