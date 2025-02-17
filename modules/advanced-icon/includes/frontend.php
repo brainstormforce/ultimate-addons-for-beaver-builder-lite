ed7e81f0a11e909dd180536f5134d0341eecfaf4
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2025-02-17 14:12:19 +0530

    phpcbf

--- modules/advanced-icon/includes/frontend.php
+++ modules/advanced-icon/includes/frontend.php
@@ -26,7 +26,7 @@ foreach ( $settings->icons as $icon ) {
 	} else {
 		if ( '' === $icon->link ) {
 			echo '<div class="adv-icon-link adv-icon-' . esc_attr( $icon_count ) . '">';
-		}else{
+		} else {
 			$link_rel = BB_Ultimate_Addon_Helper::get_link_rel( $icon->link_target, $icon->link_nofollow, 0 );
 			echo '<a class="adv-icon-link adv-icon-' . esc_attr( $icon_count ) . '" href="' . esc_url( $icon->link ) . '" target="' . esc_attr( $icon->link_target ) . '" ' . ( ! is_null( $link_rel ) ? wp_kses_post( $link_rel ) : '' ) . ' aria-label="' . esc_attr__( 'Go to', 'uabb' ) . ' ' . esc_url( $icon->link ) . '">';
 		}
