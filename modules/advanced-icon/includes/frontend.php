073f819c9acf45b21b8b34809bcb10a29ac9b9ab
Vijeta Raikar <110518282+vijetaR@users.noreply.github.com>
2025-02-25 19:01:43 +0530

    fixed icon not reflecting color on drag/drop

--- modules/advanced-icon/includes/frontend.php
+++ modules/advanced-icon/includes/frontend.php
@@ -24,12 +24,8 @@ foreach ( $settings->icons as $icon ) {
 	if ( ! empty( $icon->connections->link ) && empty( $icon->link ) && ! FLBuilderModel::is_builder_active() ) {
 		echo '';
 	} else {
-		if ( '' === $icon->link ) {
-			echo '<div class="adv-icon-link adv-icon-' . esc_attr( $icon_count ) . '">';
-		} else {
-			$link_rel = BB_Ultimate_Addon_Helper::get_link_rel( $icon->link_target, $icon->link_nofollow, 0 );
-			echo '<a class="adv-icon-link adv-icon-' . esc_attr( $icon_count ) . '" href="' . esc_url( $icon->link ) . '" target="' . esc_attr( $icon->link_target ) . '" ' . ( ! is_null( $link_rel ) ? wp_kses_post( $link_rel ) : '' ) . ' aria-label="' . esc_attr__( 'Go to', 'uabb' ) . ' ' . esc_url( $icon->link ) . '">';
-		}
+		$link_rel = sanitize_text_field( BB_Ultimate_Addon_Helper::get_link_rel( $icon->link_target, $icon->link_nofollow, 0 ) );
+		echo '<a class="adv-icon-link adv-icon-' . esc_attr( $icon_count ) . '" href="' . esc_url( $icon->link ) . '" target="' . esc_attr( $icon->link_target ) . '" ' . ( ! empty( $link_rel ) ? 'rel="' . esc_attr( $link_rel ) . '"' : '' ) . '>';
 		$imageicon_array = array(
 
 			/* General Section */
@@ -81,11 +77,7 @@ foreach ( $settings->icons as $icon ) {
 			'img_border_hover_color'  => $settings->border_hover_color,
 		);
 		FLBuilder::render_module_html( 'image-icon', $imageicon_array );
-		if ( '' === $icon->link ) {
-			echo '</div>';
-		} else {
-			echo '</a>';
-		}
+		echo '</a>';
 	}
 	++$icon_count;
 }
