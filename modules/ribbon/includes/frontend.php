5abfe4496b1a50c800e868b3ac0765f2e38e1b92
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-04-02 13:31:16 +0530

    updated final comments

--- modules/ribbon/includes/frontend.php
+++ modules/ribbon/includes/frontend.php
@@ -21,7 +21,7 @@ if ( ! isset( $settings ) ) {
 	$allowed_tags = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'p', 'span' );
 	$ribbon_tag   = in_array( $settings->text_tag_selection, $allowed_tags, true ) ? $settings->text_tag_selection : 'h3';
 ?>
-<div class="uabb-ribbon-wrap" role="banner" tabindex="0" aria-label="<?php echo esc_attr( $title ); ?>">
+<div class="uabb-ribbon-wrap" role="banner" tabindex="0" aria-label="<?php echo esc_attr( $settings->title ); ?>">
 	<<?php echo esc_attr( $ribbon_tag ); ?> class="uabb-ribbon">		
 		<span class="uabb-left-ribb flips" aria-hidden="true"><i class="<?php echo esc_attr( $settings->left_icon ); ?>"></i></span>
 		<span class="uabb-ribbon-text">
