b0e11134cca82e6161d3014105aa4d2feebbda2c
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-04-02 12:56:54 +0530

    updated commets

--- modules/ribbon/includes/frontend.php
+++ modules/ribbon/includes/frontend.php
@@ -21,7 +21,7 @@ if ( ! isset( $settings ) ) {
 	$allowed_tags = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'p', 'span' );
 	$ribbon_tag   = in_array( $settings->text_tag_selection, $allowed_tags, true ) ? $settings->text_tag_selection : 'h3';
 ?>
-<div class="uabb-ribbon-wrap" role="banner" tabindex="0">
+<div class="uabb-ribbon-wrap" role="banner" tabindex="0" title="<?php echo wp_kses_post( $settings->title ); ?>">
 	<<?php echo esc_attr( $ribbon_tag ); ?> class="uabb-ribbon">		
 		<span class="uabb-left-ribb flips"><i class="<?php echo esc_attr( $settings->left_icon ); ?>"></i></span>
 		<span class="uabb-ribbon-text">
